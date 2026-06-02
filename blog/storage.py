261cbf05821a6e1e4661f47d923196c07bb8a5cc
ramazon07-cmd <ustau2453@gmail.com>
2026-06-02 19:55:11 +0500

    bug has been fixed

--- blog/storage.py
+++ blog/storage.py
@@ -6,23 +6,23 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaRootS3Storage(S3Boto3Storage):
     location = os.environ.get("AWS_LOCATION", "media")
 
-    def _strip_location_prefix(self, name):
-        if not name:
-            return name
+    # def _strip_location_prefix(self, name):
+    #     if not name:
+    #         return name
 
-        normalized = str(name).lstrip("/")
-        location_prefix = f"{self.location.strip('/')}/"
-        while normalized.startswith(location_prefix):
-            normalized = normalized[len(location_prefix):]
-        return normalized
+    #     normalized = str(name).lstrip("/")
+    #     location_prefix = f"{self.location.strip('/')}/"
+    #     while normalized.startswith(location_prefix):
+    #         normalized = normalized[len(location_prefix):]
+    #     return normalized
 
-    def _save(self, name, content):
-        return super()._save(self._strip_location_prefix(name), content)
+    # def _save(self, name, content):
+    #     return super()._save(self._strip_location_prefix(name), content)
 
-    def url(self, name, parameters=None, expire=None, http_method=None):
-        return super().url(
-            self._strip_location_prefix(name),
-            parameters=parameters,
-            expire=expire,
-            http_method=http_method,
-        )
+    # def url(self, name, parameters=None, expire=None, http_method=None):
+    #     return super().url(
+    #         self._strip_location_prefix(name),
+    #         parameters=parameters,
+    #         expire=expire,
+    #         http_method=http_method,
+    #     )
